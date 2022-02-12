86fed12ac48f750c16efd1311dfae980371725f9
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-02-12 12:15:25 +0100

    create annotations

---
+++ src/main/java/io/polyfrost/oneconfig/annotations/Button.java
@@ -0,0 +1,14 @@
+package io.polyfrost.oneconfig.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Button {
+    String name();
+    String description() default "";
+    String text() default "Button";
+}
---
+++ src/main/java/io/polyfrost/oneconfig/annotations/Slider.java
@@ -0,0 +1,15 @@
+package io.polyfrost.oneconfig.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Slider {
+    String name();
+    String description() default "";
+    int min();
+    int max();
+}
