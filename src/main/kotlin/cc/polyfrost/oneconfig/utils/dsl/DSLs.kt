efd04bd3db899f050fbda8287da4edea3c6834ce
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-06-04 16:57:20 +0700

    packet events

---
+++ src/main/kotlin/cc/polyfrost/oneconfig/utils/dsl/DSLs.kt
@@ -0,0 +1,9 @@
+package cc.polyfrost.oneconfig.utils.dsl
+
+import cc.polyfrost.oneconfig.libs.universal.UMinecraft
+
+/**
+ * Gets the current [net.minecraft.client.Minecraft] instance.
+ */
+val mc
+get() = UMinecraft.getMinecraft()
