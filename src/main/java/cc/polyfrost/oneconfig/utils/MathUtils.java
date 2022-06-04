fab540b4e847c59fe22c3a772374e85f0890ad26
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-06-04 22:34:12 +0700

    delete easeOut

--- src/main/java/cc/polyfrost/oneconfig/utils/MathUtils.java
+++ src/main/java/cc/polyfrost/oneconfig/utils/MathUtils.java
@@ -9,16 +9,6 @@ public final class MathUtils {
         return number < min ? min : Math.min(number, max);
     }
 
-    @Deprecated
-    public static float easeOut(float current, float goal, float speed) {
-        float deltaTime = GuiUtils.getDeltaTime();
-        if (Math.round(Math.abs(goal - current) * 100) > 0) {
-            return current + (goal - current) / speed * deltaTime;
-        } else {
-            return goal;
-        }
-    }
-
     public static float map(float value, float start1, float stop1, float start2, float stop2) {
         return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
     }
