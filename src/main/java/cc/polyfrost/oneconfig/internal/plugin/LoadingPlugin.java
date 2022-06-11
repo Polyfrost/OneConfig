56b13f7fbba5550a6c77966726ab414da85f0a9b
DeDiamondPro <67508414+DeDiamondPro@users.noreply.github.com>
2022-06-11 17:35:22 +0200

    loading plugin

--- src/main/java/cc/polyfrost/oneconfig/internal/plugin/LoadingPlugin.java
+++ src/main/java/cc/polyfrost/oneconfig/internal/plugin/LoadingPlugin.java
@@ -4,18 +4,20 @@ import cc.polyfrost.oneconfig.internal.init.OneConfigInit;
 import cc.polyfrost.oneconfig.internal.plugin.asm.ClassTransformer;
 import net.minecraft.launchwrapper.Launch;
 import net.minecraft.launchwrapper.LaunchClassLoader;
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.Set;
 
-public class LoadingPlugin {
+public class LoadingPlugin implements IFMLLoadingPlugin {
 
     /**
      * Taken from LWJGLTwoPointFive under The Unlicense
      * <a href="https://github.com/DJtheRedstoner/LWJGLTwoPointFive/blob/master/LICENSE/">https://github.com/DJtheRedstoner/LWJGLTwoPointFive/blob/master/LICENSE/</a>
      */
     public LoadingPlugin() {
+        if (Launch.blackboard.containsKey("oneconfig.init.initialized")) return;
         try {
             Field f_exceptions = LaunchClassLoader.class.getDeclaredField("classLoaderExceptions");
             f_exceptions.setAccessible(true);
@@ -28,23 +30,28 @@ public class LoadingPlugin {
         }
     }
 
+    @Override
     public String[] getASMTransformerClass() {
+        if (Launch.blackboard.containsKey("oneconfig.init.registered_transformer")) return new String[]{};
         Launch.blackboard.put("oneconfig.init.registered_transformer", true);
         return new String[]{ClassTransformer.class.getName()};
     }
 
+    @Override
     public String getModContainerClass() {
         return null;
     }
 
+    @Override
     public String getSetupClass() {
         return null;
     }
 
+    @Override
     public void injectData(Map<String, Object> data) {
-
     }
 
+    @Override
     public String getAccessTransformerClass() {
         return null;
     }
