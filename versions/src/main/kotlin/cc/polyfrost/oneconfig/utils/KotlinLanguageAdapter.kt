a4ea625a6ad576a89091db74a1223f73c144d338
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-07-16 21:11:06 +0900

    Change wording from "taken" to "adapted" We make a few changes to the adapter

--- versions/src/main/kotlin/cc/polyfrost/oneconfig/utils/KotlinLanguageAdapter.kt
+++ versions/src/main/kotlin/cc/polyfrost/oneconfig/utils/KotlinLanguageAdapter.kt
@@ -12,17 +12,14 @@ import java.lang.reflect.Method
  * An adapter for FML to allow for the use of Kotlin objects as a mod class.
  * This is not required if you use a Kotlin class, only if you use a Kotlin object.
  *
- * Taken from Crimson under LGPL 3.0
+ * Adapted from Crimson under LGPL 3.0
  * https://github.com/Deftu-Archive/Crimson/blob/main/LICENSE
  */
 class KotlinLanguageAdapter : ILanguageAdapter {
 
     override fun supportsStatics(): Boolean = false
     override fun getNewInstance(
-        container: FMLModContainer,
-        objectClass: Class<*>,
-        classLoader: ClassLoader,
-        factoryMarkedAnnotation: Method
+        container: FMLModContainer, objectClass: Class<*>, classLoader: ClassLoader, factoryMarkedAnnotation: Method
     ): Any = objectClass.kotlin.objectInstance ?: objectClass.getDeclaredConstructor().newInstance()
 
     override fun setProxy(target: Field, proxyTarget: Class<*>, proxy: Any) {
