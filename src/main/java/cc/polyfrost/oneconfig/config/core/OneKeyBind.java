b05f8b28809d113741e67ce3702da46244f5d187
ForBai <thelars42@gmail.com>
2022-07-31 13:13:04 +0200

    Add documantion

--- src/main/java/cc/polyfrost/oneconfig/config/core/OneKeyBind.java
+++ src/main/java/cc/polyfrost/oneconfig/config/core/OneKeyBind.java
@@ -9,11 +9,18 @@ public class OneKeyBind {
     protected transient Runnable runnable;
     protected transient boolean hasRun;
 
+    /**
+     * @param keys  The bound keys
+     */
     public OneKeyBind(int... keys) {
         for (int key : keys) {
             keyBinds.add(key);
         }
     }
+
+    /**
+     * @return If the keys are pressed
+     */
     public boolean isActive() {
         if (keyBinds.size() == 0) return false;
         for (int keyBind : keyBinds) {
@@ -25,12 +32,18 @@ public class OneKeyBind {
         return true;
     }
 
+    /**
+     * Run the set Runnable
+     */
     public void run() {
         if (runnable == null || hasRun) return;
         runnable.run();
         hasRun = true;
     }
 
+    /**
+     * @return The set keys as the name of the keys
+     */
     public String getDisplay() {
         StringBuilder sb = new StringBuilder();
         for (int keyBind : keyBinds) {
@@ -40,22 +53,38 @@ public class OneKeyBind {
         return sb.toString().trim();
     }
 
+    /**
+     * @param key   Add a Key to keys
+     */
     public void addKey(int key) {
         if (!keyBinds.contains(key)) keyBinds.add(key);
     }
 
+    /**
+     * Clear the keys List
+     */
     public void clearKeys() {
         keyBinds.clear();
     }
 
+    /**
+     * @return The amount of key in the keys List
+     */
     public int getSize() {
         return keyBinds.size();
     }
 
+    /**
+     * Set the Runnable that gets ran when OneKeyBind#run() is called
+     * @param runnable The Runnable to run
+     */
     public void setRunnable(Runnable runnable) {
         this.runnable = runnable;
     }
 
+    /**
+     * @return The key in the keys List
+     */
     public ArrayList<Integer> getKeyBinds() {
         return keyBinds;
     }
