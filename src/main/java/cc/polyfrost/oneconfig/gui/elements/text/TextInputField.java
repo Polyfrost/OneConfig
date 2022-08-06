fd50beea9325611a458c431123358eb72f07ad64
nxtdaydelivery <12willettsh@gmail.com>
2022-08-06 13:10:36 +0100

    almost done now

--- src/main/java/cc/polyfrost/oneconfig/gui/elements/text/TextInputField.java
+++ src/main/java/cc/polyfrost/oneconfig/gui/elements/text/TextInputField.java
@@ -3,6 +3,7 @@ package cc.polyfrost.oneconfig.gui.elements.text;
 import cc.polyfrost.oneconfig.gui.elements.BasicElement;
 import cc.polyfrost.oneconfig.internal.assets.Colors;
 import cc.polyfrost.oneconfig.libs.universal.UKeyboard;
+import cc.polyfrost.oneconfig.platform.Platform;
 import cc.polyfrost.oneconfig.renderer.RenderManager;
 import cc.polyfrost.oneconfig.renderer.SVG;
 import cc.polyfrost.oneconfig.renderer.font.Fonts;
@@ -11,23 +12,23 @@ import cc.polyfrost.oneconfig.utils.InputUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.geom.Point2D;
-import java.util.Arrays;
 
 public class TextInputField extends BasicElement {
-    public final int DOWN_LINE = Integer.MIN_VALUE;
-    public final int UP_LINE = Integer.MAX_VALUE;
-    public final int MOVE_END = Integer.MAX_VALUE - 1;
-    public final int MOVE_START = Integer.MIN_VALUE + 1;
+    public static final int DOWN_LINE = Integer.MIN_VALUE;
+    public static final int UP_LINE = Integer.MAX_VALUE;
+    public static final int MOVE_END = Integer.MAX_VALUE - 1;
+    public static final int MOVE_START = Integer.MIN_VALUE + 1;
     private final int LINE_HEIGHT = 20, MAX_WIDTH;
     private static final String NEW_LINE = "/n";
     protected final String defaultText;
     protected final SVG icon;
     protected final boolean centered, multiLine, password;
+    protected boolean mouseWasDown, dragging;
     protected String input, renderCache;
     protected Selection selection;
     protected int caretPos;
     protected boolean errored, shown;
-    private long vg;
+    private long vg, clickTime, clickTime2;
     private Point2D.Float textStart;
     private float x;
 
@@ -38,6 +39,7 @@ public class TextInputField extends BasicElement {
         this.password = password;
         this.input = "";
         this.icon = icon;
+        // TODO centering (just use textStart)
         this.centered = centered;
         this.MAX_WIDTH = width - (icon != null ? 32 : 12);
     }
@@ -71,7 +73,7 @@ public class TextInputField extends BasicElement {
         // DRAW BOX AND ICON
         int colorOutline = errored ? Colors.ERROR_700 : Colors.GRAY_700;
         if (!toggled)
-            RenderManager.drawHollowRoundRect(vg, x, y, width - 0.5f, height - 0.5f, colorOutline, 12f, 2f);
+            RenderManager.drawHollowRoundRect(vg, x, y, width - 0.5f, height - 0.5f, colorOutline, 12f, 2f);        // TODO box expansion
         else {
             RenderManager.setAlpha(vg, 0.15f);
             RenderManager.drawRoundedRect(vg, x - 4, y - 4, width + 8, height + 8, errored ? Colors.ERROR_600 : Colors.PRIMARY_600, 16);
@@ -85,6 +87,11 @@ public class TextInputField extends BasicElement {
         }
 
         // check the selection
+        if (input.equals("")) {
+            // draw empty caret
+            RenderManager.drawRect(vg, textStart.x, textStart.y - 10, 1, LINE_HEIGHT, Colors.WHITE);
+            return;
+        }
         float selectionLength = 0;
         boolean longSelection = false;
         validateSelection();
@@ -96,12 +103,12 @@ public class TextInputField extends BasicElement {
         // ACTUAL DRAWING
         float textY = textStart.y;
         int startIndex = 0;
-        for (String s : getRenderText()) {
-            int endIndex = startIndex + s.length();
+        for (final String s : getRenderText()) {
+            final int endIndex = startIndex + s.length();
 
-            // draw selection           (intellisense doesn't understand, so I have to check it again)
+            // draw selection
             if (selectionLength != 0 && selection != null) {
-                float from = selection.indexStart < endIndex && selection.indexStart >= startIndex ? getTextWidth(s.substring(0, selection.indexStart - startIndex)) : 0;
+                final float from = selection.indexStart < endIndex && selection.indexStart >= startIndex ? getTextWidth(s.substring(0, selection.indexStart - startIndex)) : 0;
                 float thisLength = selectionLength;
                 if (longSelection) {
                     thisLength = getTextWidth(s);
@@ -117,7 +124,8 @@ public class TextInputField extends BasicElement {
 
             // draw caret
             if (selection == null && toggled) {
-                if (caretPos >= startIndex && caretPos < endIndex) {
+                // check if caret is applicable for this line
+                if ((caretPos > startIndex && caretPos <= endIndex)) {
                     RenderManager.drawRect(vg, textStart.x + getTextWidth(s.substring(0, caretPos - startIndex)), textY - 10, 1, LINE_HEIGHT, Colors.WHITE);
                 }
             }
@@ -133,45 +141,68 @@ public class TextInputField extends BasicElement {
     @Override
     public void update(float x, float y) {
         super.update(x, y);
-        // todo broken! need a reliable way to distinguish between drag, click, double click, etc.
-        /*if (clicked) {
+        boolean isMouseDown = Platform.getMousePlatform().isButtonDown(0);
+        boolean hovered = Platform.getMousePlatform().isButtonDown(0) && this.hovered;
+        boolean doubleClick = false;
+        // double click
+        if (clicked && System.currentTimeMillis() - clickTime < 200 && System.currentTimeMillis() - clickTime2 < 200) {
             putCaret(snapToChar(InputUtils.mouseX(), InputUtils.mouseY()));
+            int last = input.lastIndexOf(" ", caretPos);
+            int next = input.indexOf(" ", caretPos);
+            selection = new Selection(last == -1 ? 0 : last + 1, next == -1 ? input.length() : next);
+            doubleClick = true;
             toggled = true;
-        } else if (pressed) {
-            int pos = snapToChar(InputUtils.mouseX(), InputUtils.mouseY());
+        }
+        // drag logic stuff
+        if (hovered && isMouseDown && !mouseWasDown) {
+            if (selection != null) selection = null;
+            clickTime = System.currentTimeMillis();
+            dragging = true;
+        }
+        // single click
+        if (clicked && System.currentTimeMillis() - clickTime < 200 && !doubleClick) {
+            if (!toggled) toggled = true;
+            putCaret(snapToChar(InputUtils.mouseX(), InputUtils.mouseY()));
+            clickTime2 = System.currentTimeMillis();
+        }
+        // drag
+        if (dragging && System.currentTimeMillis() - clickTime > 100 && !doubleClick) {
+            // TODO dragging
+            /*int pos = snapToChar(InputUtils.mouseX(), InputUtils.mouseY());
+            if (pos > input.length() || pos < 0) return;
             if (selection == null) {
-                if (pos != -1) {
-                    selection = new Selection(pos, 0);
-                }
+                putCaret(pos);
+                selection = new Selection(caretPos, 0);
             } else {
-                if(pos < selection.indexStart) {
+                if (caretPos > pos) {
                     selection.indexStart = pos;
                 } else {
                     selection.indexEnd = pos;
                 }
-            }
-        }*/
-
+            }*/
+        }
+        // reset on outside click
         if (InputUtils.isClicked() && !InputUtils.isAreaHovered(x - 40, y - 20, width + 90, height + 20)) {
             close();
             toggled = false;
         }
-    }
 
-    @Override
-    public void onClick() {
-        //toggled = true;
-    }
-
-    private void onDoubleClick() {
-
+        // reset
+        mouseWasDown = Platform.getMousePlatform().isButtonDown(0);
+        if (dragging && InputUtils.isClicked(true)) {
+            dragging = false;
+        }
     }
 
     // getting selected coords
     private int snapToChar(float mouseX, float mouseY) {
         String[] lines = getRenderText();
         int lineIndex = getHoveredLineIndex(mouseY);
-        if (lineIndex >= lines.length || lineIndex < 0) return -1;
+        if (lineIndex >= lines.length) {
+            return input.length();
+        } else if(lineIndex < 0) {
+            return 0;
+        }
         int index = 0;
         for (int i = 0; i < lineIndex; i++) {
             index += lines[i].length();
@@ -356,20 +387,41 @@ public class TextInputField extends BasicElement {
         if (!select && selection != null) {
             selection = null;
         }
-        // TODO works but likes to wander around the line
         if (movement == DOWN_LINE || movement == UP_LINE) {
-            String lines = getUnformattedRenderText();
-            int startOfCurrentLine = lines.substring(0, caretPos).lastIndexOf(NEW_LINE);
-            if (startOfCurrentLine == -1) startOfCurrentLine = 0;
-            startOfCurrentLine += NEW_LINE.length();
-            //System.out.println(input.substring(startOfCurrentLine, caretPos));
-            float predictedX = textStart.x + getTextWidth(lines.substring(startOfCurrentLine, caretPos)) + 0.7f;
-            float predictedY = textStart.y + lines.substring(0, caretPos).split(NEW_LINE).length * LINE_HEIGHT - (movement == DOWN_LINE ? 0 : LINE_HEIGHT * 2);
-            putCaret(snapToChar(predictedX, predictedY) + 1);
-            //System.out.println(input.substring(0, snapToChar(predictedX, predictedY)));
-            //System.out.println(InputUtils.mouseX() + " " + InputUtils.mouseY());
-            //System.out.println("predicted: " + predictedX + " " + predictedY);
-            //System.out.println("makes: " + input.substring(0, caretPos));
+            final String[] lines = getRenderText();
+            int curr = 0;
+            int i = 0;
+            for (final String line : lines) {
+                if (curr + line.length() > caretPos) {
+                    break;
+                }
+                curr += line.length() + 1;
+                i++;
+            }
+            if (i < 0) return;
+            final float currentWidth = getTextWidth(lines[i].substring(0, caretPos - curr));
+            i = (movement == DOWN_LINE) ? i + 1 : i - 1;
+            if (i < 0) {
+                moveCaret(-caretPos, select);
+                return;
+            }
+            if (i >= lines.length) {
+                moveCaret(input.length() - caretPos, select);
+                return;
+            }
+            final StringBuilder builder = new StringBuilder();
+            for (final char c : lines[i].toCharArray()) {
+                builder.append(c);
+                final float width = getTextWidth(builder.toString());
+                if (width > currentWidth) {
+                    if (movement == UP_LINE) {
+                        moveCaret(builder.length() - 1 - caretPos, select);
+                    } else {
+                        moveCaret(lines[i - 1].length() - caretPos + builder.length(), select);
+                    }
+                    return;
+                }
+            }
         } else if (movement == MOVE_END) {
             if (select) {
                 selection = new Selection(caretPos, input.length());
@@ -404,14 +456,13 @@ public class TextInputField extends BasicElement {
         moveCaret(movement, false);
     }
 
-    private void putCaret(int pos) {
+    public void putCaret(int pos) {
         if (pos < 0) caretPos = 0;
         else caretPos = Math.min(pos, input.length());
     }
 
     // render text
     public String[] getRenderText() {
-        System.out.println(Arrays.toString(getUnformattedRenderText().split(NEW_LINE)));
         return getUnformattedRenderText().split(NEW_LINE);
     }
 
@@ -422,9 +473,9 @@ public class TextInputField extends BasicElement {
             float width = 0;
             for (String word : words) {
                 width += getTextWidth(word + " ");
-                if (getTextWidth(word) >= MAX_WIDTH) {
-                    // TODO skill issue ngl
-                }
+                //if (getTextWidth(word) >= MAX_WIDTH) {
+                // TODO skill issue ngl
+                //}
                 if (width >= MAX_WIDTH) {
                     width = 0;
                     output.append(NEW_LINE);
@@ -504,6 +555,7 @@ public class TextInputField extends BasicElement {
     }
 
 
+    // selection
     private void validateSelection() {
         if (selection == null) return;
         // done like this because of debugging and it's so much easier to read
@@ -511,7 +563,7 @@ public class TextInputField extends BasicElement {
             selection = null;
         } else if (selection.indexStart > input.length() || selection.indexEnd > input.length()) {
             selection = null;
-        } else if (selection.indexEnd == selection.indexStart) {
+        } else if (selection.indexEnd == selection.indexStart && !dragging) {
             selection = null;
         }
     }
