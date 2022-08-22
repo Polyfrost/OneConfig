0fdee322119e31e86df76554b962f5c9b03b3430
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-08-22 21:42:55 +0700

    new: add license header

--- versions/1.16.2-forge/src/main/java/cc/polyfrost/oneconfig/internal/mixin/commands/ClientSuggestionProviderMixin.java
+++ versions/1.16.2-forge/src/main/java/cc/polyfrost/oneconfig/internal/mixin/commands/ClientSuggestionProviderMixin.java
@@ -29,6 +29,32 @@
  * As per the terms of the Apache 2.0 License, a copy of the License
  * is found at `src/main/resources/licenses/Fabric-License.txt`.
  */
+
+/*
+ * This file is part of OneConfig.
+ * OneConfig - Next Generation Config Library for Minecraft: Java Edition
+ * Copyright (C) 2021, 2022 Polyfrost and FabricMC.
+ *   <https://polyfrost.cc> <https://github.com/Polyfrost/>
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ *   OneConfig is licensed under the terms of version 3 of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, AND
+ * under the Additional Terms Applicable to OneConfig, as published by Polyfrost,
+ * either version 1.0 of the Additional Terms, or (at your option) any later
+ * version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ * License.  If not, see <https://www.gnu.org/licenses/>. You should
+ * have also received a copy of the Additional Terms Applicable
+ * to OneConfig, as published by Polyfrost. If not, see
+ * <https://polyfrost.cc/legal/oneconfig/additional-terms>
+ */
 package cc.polyfrost.oneconfig.internal.mixin.commands;
 
 import cc.polyfrost.oneconfig.utils.commands.FabricClientCommandSource;
