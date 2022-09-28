f96c5b2b267059d3dd4f532143d57589ae44ae1c
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-09-28 08:24:19 -0400

    Update EaseOutBump.java

--- src/main/java/cc/polyfrost/oneconfig/gui/animations/EaseOutBump.java
+++ src/main/java/cc/polyfrost/oneconfig/gui/animations/EaseOutBump.java
@@ -1,6 +1,8 @@
 package cc.polyfrost.oneconfig.gui.animations;
 
 public class EaseOutBump extends Animation {
+    private static final double CONSTANT_1 = 1.7;
+    private static final double CONSTANT_2 = 2.7;
 
     /**
      * @param duration The duration of the animation
@@ -15,10 +17,7 @@ public class EaseOutBump extends Animation {
     // Courtesy of https://easings.net/
     @Override
     protected float animate(float x) {
-        double constant1 = 1.7;
-        double constant2 = constant1 + 1;
-
         // return x == 0 ? 0 : (float) (x == 1 ? 1 : Math.pow(2, -0.5 * x) * Math.sin((x * 100 - 2) * c4) + 1);
-        return (float) (1 + constant2 * Math.pow(x-1, 3) + constant1 * 1.2 * Math.pow(x-1, 2));
+        return (float) (1 + CONSTANT_2 * Math.pow(x-1, 3) + CONSTANT_1 * 1.2 * Math.pow(x-1, 2));
     }
 }
