89a49fda12e005b0d0173f8d854bd11ccc7be3bb
nxtdaydelivery <12willettsh@gmail.com>
2022-10-06 18:47:39 +0100

    like a damn sociopath

--- versions/1.16.2-forge/src/main/java/cc/polyfrost/oneconfig/utils/commands/PlatformCommandManagerImpl.java
+++ versions/1.16.2-forge/src/main/java/cc/polyfrost/oneconfig/utils/commands/PlatformCommandManagerImpl.java
@@ -26,16 +26,9 @@
 
 package cc.polyfrost.oneconfig.utils.commands;
 
-import java.util.Collection;
-
 public class PlatformCommandManagerImpl extends PlatformCommandManager {
     @Override
     void createCommand(CommandManager.OCCommand command) {
 
-    }
-
-    @Override
-    Collection<String> getPlayerNames() {
-        return null;
     }
 }
