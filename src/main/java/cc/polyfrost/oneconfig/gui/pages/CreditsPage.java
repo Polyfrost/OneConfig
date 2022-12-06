22a00e15ebcae1ae88f15a027b95f9cb368abd6f
xtrm <oss@xtrm.me>
2022-12-06 23:09:43 +0100

    fix(main): better descriptions

--- src/main/java/cc/polyfrost/oneconfig/gui/pages/CreditsPage.java
+++ src/main/java/cc/polyfrost/oneconfig/gui/pages/CreditsPage.java
@@ -43,13 +43,13 @@ public class CreditsPage extends Page {
         y -= 32;
 
         nanoVGHelper.drawText(vg, "Development Team", x + 20, y + 180, -1, 24, Fonts.SEMIBOLD);
-        nanoVGHelper.drawText(vg, " - MoonTidez - Founder and lead designer", x + 20, y + 205, -1, 12, Fonts.REGULAR);
-        nanoVGHelper.drawText(vg, " - DeDiamondPro - Founder, Config backend, GUI frontend, HUD", x + 20, y + 220, -1, 12, Fonts.REGULAR);        // +15/line
-        nanoVGHelper.drawText(vg, " - nextdaydelivery - GUI frontend, Render Manager, Utilities", x + 20, y + 235, -1, 12, Fonts.REGULAR);
-        nanoVGHelper.drawText(vg, " - Wyvest - Gradle, Render Manager, VCAL, Utilities", x + 20, y + 250, -1, 12, Fonts.REGULAR);
+        nanoVGHelper.drawText(vg, " - MoonTidez - Founder and Lead Designer", x + 20, y + 205, -1, 12, Fonts.REGULAR);
+        nanoVGHelper.drawText(vg, " - DeDiamondPro - Founder, Config Backend, GUI Frontend, HUD", x + 20, y + 220, -1, 12, Fonts.REGULAR);        // +15/line
+        nanoVGHelper.drawText(vg, " - nextdaydelivery - GUI Frontend, NanoVGHelper, Utilities", x + 20, y + 235, -1, 12, Fonts.REGULAR);
+        nanoVGHelper.drawText(vg, " - Wyvest - Gradle, NanoVGHelper, VCAL, Utilities", x + 20, y + 250, -1, 12, Fonts.REGULAR);
         nanoVGHelper.drawText(vg, " - Pauline - Utilities", x + 20, y + 265, -1, 12, Fonts.REGULAR);
         nanoVGHelper.drawText(vg, " - Caledonian - Lead Designer", x + 20, y + 280, -1, 12, Fonts.REGULAR);
-        nanoVGHelper.drawText(vg, " - xtrm - Legacy/Modern hacky compatibility", x + 20, y + 295, -1, 12, Fonts.REGULAR);
+        nanoVGHelper.drawText(vg, " - xtrm - Multiversion, GUI Frontend", x + 20, y + 295, -1, 12, Fonts.REGULAR);
 
         nanoVGHelper.drawText(vg, "Libraries", x + 20, y + 333, -1, 24, Fonts.SEMIBOLD);
         nanoVGHelper.drawText(vg, " - LWJGLTwoPointFive (DJTheRedstoner) - LWJGL2 function provider", x + 20, y + 355, -1, 12, Fonts.REGULAR);
