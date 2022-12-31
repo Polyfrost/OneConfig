bdceed34248911f99db43a2aee159bb8e092b06f
Wyvest <45589059+Wyvest@users.noreply.github.com>
2022-12-31 14:31:57 +0900

    new: change font to poppins

--- src/main/java/cc/polyfrost/oneconfig/renderer/font/Fonts.java
+++ src/main/java/cc/polyfrost/oneconfig/renderer/font/Fonts.java
@@ -27,10 +27,10 @@
 package cc.polyfrost.oneconfig.renderer.font;
 
 public class Fonts {
-    public static final Font BOLD = new Font("inter-bold", "/assets/oneconfig/font/Bold.otf");
-    public static final Font SEMIBOLD = new Font("inter-semibold", "/assets/oneconfig/font/SemiBold.otf");
-    public static final Font MEDIUM = new Font("inter-medium", "/assets/oneconfig/font/Medium.otf");
-    public static final Font REGULAR = new Font("inter-regular", "/assets/oneconfig/font/Regular.otf");
+    public static final Font BOLD = new Font("poppins-bold", "/assets/oneconfig/font/Bold.ttf");
+    public static final Font SEMIBOLD = new Font("poppins-semibold", "/assets/oneconfig/font/SemiBold.ttf");
+    public static final Font MEDIUM = new Font("poppins-medium", "/assets/oneconfig/font/Medium.ttf");
+    public static final Font REGULAR = new Font("poppins-regular", "/assets/oneconfig/font/Regular.ttf");
     public static final Font MINECRAFT_REGULAR = new Font("mc-regular", "/assets/oneconfig/font/Minecraft-Regular.otf");
     public static final Font MINECRAFT_BOLD = new Font("mc-bold", "/assets/oneconfig/font/Minecraft-Bold.otf");
 }
