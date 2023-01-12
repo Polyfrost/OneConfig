4219ed5523a1bb18f0b8bbf37579d6dd19d033ee
xtrm <oss@xtrm.me>
2023-01-12 14:56:15 +0100

    test: added test page for category overload

---
+++ versions/src/main/java/cc/polyfrost/oneconfig/test/TestPage2_Test.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of OneConfig.
+ * OneConfig - Next Generation Config Library for Minecraft: Java Edition
+ * Copyright (C) 2021, 2022 Polyfrost.
+ *   <https://polyfrost.cc> <https://github.com/Polyfrost/>
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ *   OneConfig is licensed under the terms of version 3 of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, AND
+ * under the Additional Terms Applicable to OneConfig, as published by Polyfrost,
+ * either version 1.0 of the Additional Terms, or (at your option) any later
+ * version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ * License.  If not, see <https://www.gnu.org/licenses/>. You should
+ * have also received a copy of the Additional Terms Applicable
+ * to OneConfig, as published by Polyfrost. If not, see
+ * <https://polyfrost.cc/legal/oneconfig/additional-terms>
+ */
+
+package cc.polyfrost.oneconfig.test;
+
+import cc.polyfrost.oneconfig.config.annotations.Switch;
+
+public class TestPage2_Test {
+
+    @Switch(
+            name = "Epic Test Switch",
+            category = "Category 1"
+    )
+    boolean test = false;
+
+    @Switch(
+            name = "Epic Test Switch 2",
+            category = "Category 2"
+    )
+    boolean test2 = false;
+
+    @Switch(
+            name = "Epic Test Switch 3",
+            category = "Category 3"
+    )
+    boolean test3 = false;
+
+    @Switch(
+            name = "Epic Test Switch 4",
+            category = "Category 4"
+    )
+    boolean test4 = false;
+
+    @Switch(
+            name = "Epic Test Switch 5",
+            category = "Category 5"
+    )
+    boolean test5 = false;
+
+    @Switch(
+            name = "Epic Test Switch 6",
+            category = "Category 6"
+    )
+    boolean test6 = false;
+
+    @Switch(
+            name = "Epic Test Switch 7",
+            category = "Category 7"
+    )
+    boolean test7 = false;
+
+    @Switch(
+            name = "Epic Test Switch 8",
+            category = "Category 8"
+    )
+    boolean test8 = false;
+
+    @Switch(
+            name = "Epic Test Switch 9",
+            category = "cat9"
+    )
+    boolean test9 = false;
+}
