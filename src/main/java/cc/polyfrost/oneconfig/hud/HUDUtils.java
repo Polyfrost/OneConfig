86c1c8e1c1ff4bb5f5e140e87268b718e383b724
ImToggle <98242902+ImToggle@users.noreply.github.com>
2024-04-23 22:02:40 +0800

    real fix

--- src/main/java/cc/polyfrost/oneconfig/hud/HUDUtils.java
+++ src/main/java/cc/polyfrost/oneconfig/hud/HUDUtils.java
@@ -74,8 +74,10 @@ public class HUDUtils {
             options.add(new ConfigHeader(field, hud, hudAnnotation.name(), category, subcategory, 2));
             options.add(new ConfigSwitch(fields.get("enabled"), hud, "Enabled", "If the HUD is enabled", category, subcategory, 1));
             options.add(new ConfigButton(fields.get("resetPosition"), hud, "Position", "Reset HUD to default position", category, subcategory, 1, "Reset"));
-            options.add(new ConfigSwitch(fields.get("locked"), hud, "Locked", "If the position is locked", category, subcategory, HudCore.isPatcher ? 1 : 2));
-            if (HudCore.isPatcher) options.add(new ConfigSwitch(fields.get("ignoreCaching"), hud, "Ignore HUD Caching", "Ignore Patcher's HUD Caching feature, which limits HUDs to a specific FPS.", category, subcategory, 1));
+            options.add(new ConfigSwitch(fields.get("locked"), hud, "Locked", "If the position is locked", category, subcategory, 1));
+            BasicOption ignoreCaching = new ConfigSwitch(fields.get("ignoreCaching"), hud, "Ignore HUD Caching", "Ignore Patcher's HUD Caching feature, which limits HUDs to a specific FPS.", category, subcategory, 1);
+            ignoreCaching.addHideCondition(()-> !HudCore.isPatcher());
+            options.add(ignoreCaching);
             options.add(new ConfigSlider(fields.get("scale"), hud, "Scale", "The scale of the HUD", category, subcategory, 0.3f, 10f, 0, false));
             ConfigDropdown dropdown = new ConfigDropdown(fields.get("positionAlignment"), hud, "Position Alignment", "The alignment of the HUD", category, subcategory, 2, new String[]{"Auto", "Left", "Center", "Right"});
             dropdown.addListener(() -> hud.setScale(hud.scale, Platform.getGuiPlatform().getCurrentScreen() instanceof HudGui));
