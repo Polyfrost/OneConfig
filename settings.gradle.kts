4dc65b85a7a3098e9f1b5eb92656216bf21514df
ev chang <wyvestbusiness@gmail.com>
2024-04-30 14:23:25 -0400

    Bump PGT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://repo.polyfrost.org/releases")
     }
-    val pgtVersion = "0.4.4"
+    val pgtVersion = "0.5.0"
     plugins {
         id("org.polyfrost.multi-version.root") version pgtVersion
         id("org.polyfrost.defaults") version pgtVersion
