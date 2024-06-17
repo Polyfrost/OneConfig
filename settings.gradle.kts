2dcb715e2d09b567eef7546946b67f253923cece
ev chang <wyvestbusiness@gmail.com>
2024-06-18 03:58:04 +0700

    update PGT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://repo.polyfrost.org/releases")
     }
-    val pgtVersion = "0.6.0"
+    val pgtVersion = "0.6.2"
     plugins {
         id("org.polyfrost.multi-version.root") version pgtVersion
         id("org.polyfrost.defaults") version pgtVersion
