67ad7d6b4d6b28a5a2946741d3e77a71028639c0
Deftu <me@deftu.dev>
2024-09-30 21:25:59 +0200

    Update v0 dependency to alpha216

--- versions/build.gradle.kts
+++ versions/build.gradle.kts
@@ -69,7 +69,7 @@ dependencies {
             isTransitive = false
         }
         implementation("org.ow2.asm:asm-debug-all:5.2")
-        compileOnly("cc.polyfrost:oneconfig-${platform}:0.2.2-alpha195") {
+        compileOnly("cc.polyfrost:oneconfig-${platform}:0.2.2-alpha216") {
             isTransitive = false
         }
     }
