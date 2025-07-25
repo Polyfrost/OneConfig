07ed310cd8593761640ca2f32180a2ad1f9cefee
Mona <59416038+meowora@users.noreply.github.com>
2025-07-25 23:02:49 +0200

    unprivatize the extension properties

--- minecraft/src/main/kotlin/org/polyfrost/oneconfig/utils/v1/dsl/treeExtensions.kt
+++ minecraft/src/main/kotlin/org/polyfrost/oneconfig/utils/v1/dsl/treeExtensions.kt
@@ -7,30 +7,30 @@ import org.polyfrost.oneconfig.api.config.v1.Visualizer
 import org.polyfrost.polyui.data.PolyImage
 
 @Suppress("UNCHECKED_CAST")
-internal var Property<*>.visualizer: Class<out Visualizer>?
+var Property<*>.visualizer: Class<out Visualizer>?
     get() = metadata?.get("visualizer") as? Class<out Visualizer>
     set(value) = addMetadata("visualizer", value)
 
-internal var Node.category: String?
+var Node.category: String?
     get() = metadata?.get("category") as? String
     set(value) = addMetadata("category", value)
 
-internal var Node.subcategory: String?
+var Node.subcategory: String?
     get() = metadata?.get("subcategory") as? String
     set(value) = addMetadata("subcategory", value)
 
-internal var Node.index: Int?
+var Node.index: Int?
     get() = metadata?.get("index") as? Int
     set(value) = addMetadata("index", value)
 
-internal var Node.icon: PolyImage?
+var Node.icon: PolyImage?
     get() = metadata?.get("icon") as? PolyImage
     set(value) = addMetadata("icon", value)
 
-internal var Tree.saveFunction: Runnable?
+var Tree.saveFunction: Runnable?
     get() = metadata?.get("custom_save") as? Runnable
     set(value) = addMetadata("custom_save", value)
 
-internal var Tree.noCache: Boolean?
+var Tree.noCache: Boolean?
     get() = metadata?.get("no_cache") as? Boolean
     set(value) = addMetadata("no_cache", value)
