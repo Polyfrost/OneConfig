d836b354a5a99e669cdc749654514860197d63d9
Wyvest <wyvestbusiness@gmail.com>
2025-09-21 01:23:06 -0400

    Cut down mc versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -88,58 +88,25 @@ project(":bootstrap").buildFileName = "root.gradle.kts"
 listOf(
     "1.8.9-forge",
     "1.8.9-fabric",
-
     "1.12.2-forge",
     "1.12.2-fabric",
-
     "1.16.5-forge",
     "1.16.5-fabric",
-
-    "1.17.1-forge",
-    "1.17.1-fabric",
-
-    "1.18.2-forge",
-    "1.18.2-fabric",
-
-    "1.19.2-forge",
-    "1.19.2-fabric",
-
-    "1.19.4-forge",
-    "1.19.4-fabric",
-
     "1.20.1-forge",
     "1.20.1-fabric",
-
     "1.20.4-forge",
     "1.20.4-neoforge",
     "1.20.4-fabric",
-
-    "1.20.6-neoforge",
-    "1.20.6-fabric",
-
     "1.21.1-neoforge",
     "1.21.1-fabric",
-
-    "1.21.2-neoforge",
-    "1.21.2-fabric",
-
-    "1.21.3-neoforge",
-    "1.21.3-fabric",
-
     "1.21.4-neoforge",
     "1.21.4-fabric",
-
     "1.21.5-neoforge",
     "1.21.5-fabric",
-
-    "1.21.6-neoforge",
-    "1.21.6-fabric",
-
-    "1.21.7-neoforge",
-    "1.21.7-fabric",
-
     "1.21.8-neoforge",
-    "1.21.8-fabric"
+    "1.21.8-fabric",
+    "1.21.9-neoforge",
+    "1.21.9-fabric",
 ).forEach { version ->
     val proj = ":minecraft:$version"
     include(proj)
@@ -153,8 +120,6 @@ listOf(
             "1.16.5-forge",
             "1.16.5-fabric",
             "1.21.1-fabric",
-            "1.21.2-fabric",
-            "1.21.3-fabric",
             "1.21.4-fabric",
             "1.21.5-fabric").contains(version)) {
         include(bootstrapProj)
