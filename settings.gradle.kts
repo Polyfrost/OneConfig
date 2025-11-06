71401d55e8978086d0da7714953c09bf87b258bb
Deftu <me@deftu.dev>
2025-11-06 06:39:48 +0200

    Bump DGT, Textile and OmniCore

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,7 +24,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version("2.2.10")
-        id("dev.deftu.gradle.multiversion-root") version("2.59.0") // Update in libs.versions.toml too!!!
+        id("dev.deftu.gradle.multiversion-root") version("2.61.0") // Update in libs.versions.toml too!!!
     }
 }
 
