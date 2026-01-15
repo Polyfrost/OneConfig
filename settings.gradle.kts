15f0ea9e5b8fa72772bebb026c5c3bd308070b93
Wyvest <wyvestbusiness@gmail.com>
2026-01-15 02:24:13 -0500

    bye 1.8 pt 1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -80,38 +80,10 @@ project(":minecraft").buildFileName = "root.gradle.kts"
 include(":bootstrap")
 project(":bootstrap").buildFileName = "root.gradle.kts"
 listOf(
-    "1.8.9-forge",
-    "1.8.9-fabric",
-
-    "1.12.2-forge",
-    "1.12.2-fabric",
-
-    "1.16.5-forge",
-    "1.16.5-fabric",
-
-    "1.20.1-forge",
-    "1.20.1-fabric",
-
-    "1.20.4-forge",
-    "1.20.4-neoforge",
-    "1.20.4-fabric",
-
-    "1.21.1-neoforge",
     "1.21.1-fabric",
-
-    "1.21.4-neoforge",
     "1.21.4-fabric",
-
-    "1.21.5-neoforge",
-    "1.21.5-fabric",
-
-    "1.21.8-neoforge",
     "1.21.8-fabric",
-
-    "1.21.10-neoforge",
     "1.21.10-fabric",
-
-    "1.21.11-neoforge",
     "1.21.11-fabric",
 ).forEach { version ->
     val proj = ":minecraft:$version"
@@ -121,13 +93,8 @@ listOf(
         buildFileName = "../../build.gradle.kts"
     }
     val bootstrapProj = ":bootstrap:bootstrap-$version"
-    if (listOf("1.8.9-forge",
-            "1.12.2-forge",
-            "1.16.5-forge",
-            "1.16.5-fabric",
-            "1.21.1-fabric",
+    if (listOf("1.21.1-fabric",
             "1.21.4-fabric",
-            "1.21.5-fabric",
             "1.21.8-fabric",
             "1.21.10-fabric",
             "1.21.11-fabric",
