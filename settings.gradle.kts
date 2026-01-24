309e9655c1b323cead22f9888f211e6bb475e08e
Wyvest <wyvestbusiness@gmail.com>
2026-01-24 13:27:33 -0500

    Add version '26.1-fabric' to settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -133,6 +133,7 @@ listOf(
             "1.21.8-fabric",
             "1.21.10-fabric",
             "1.21.11-fabric",
+            "26.1-fabric"
         ).contains(version)) {
         include(bootstrapProj)
         project(bootstrapProj).apply {
