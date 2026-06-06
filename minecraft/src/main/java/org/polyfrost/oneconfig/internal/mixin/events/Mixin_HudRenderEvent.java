9ffa6a1ff70f5d2262923182d554e019fc6fc168
Julian Chang <wyvest@polyfrost.org>
2026-06-06 12:31:28 +0700

    Fix HUD mixin on 26.2

--- minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_HudRenderEvent.java
+++ minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_HudRenderEvent.java
@@ -2,7 +2,11 @@ package org.polyfrost.oneconfig.internal.mixin.events;
 
 //~ gui_graphics
 import net.minecraft.client.DeltaTracker;
+//? >= 26.2 {
+//import net.minecraft.client.gui.Hud;
+//? } else {
 import net.minecraft.client.gui.Gui;
+//? }
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import org.polyfrost.oneconfig.internal.OneConfig;
 import org.polyfrost.oneconfig.internal.ui.compose.SkiaCtx;
@@ -11,7 +15,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+//? if >= 26.2 {
+// @Mixin(Hud.class)
+//? } else {
 @Mixin(Gui.class)
+//? }
 public class Mixin_HudRenderEvent {
 
     //~ if >= 26.1 'render' -> 'extractRenderState'
