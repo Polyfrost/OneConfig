4ea4a494954f51b0e52291ba3244465b0a3db1b2
Mona <git@mona.cat>
2026-06-15 15:55:29 +0200

    fix: mouse input event not working
    Closes: #671

--- minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_MouseInputEvent.java
+++ minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_MouseInputEvent.java
@@ -9,16 +9,14 @@ import org.polyfrost.oneconfig.api.event.v1.events.MouseInputEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MouseHandler.class)
 public class Mixin_MouseInputEvent {
     //? >= 1.21.10 {
-    @ModifyVariable(method = "onButton", at = @At("STORE"), ordinal = 0)
-    private int mouseCallback(int button, long handle, MouseButtonInfo event, int action) {
-        EventManager.INSTANCE.post(new MouseInputEvent(button, action));
-        return button;
+    @Inject(method = "onButton", at = @At("HEAD"))
+    private void mouseCallback(long window, MouseButtonInfo buttonInfo, int action, CallbackInfo ci) {
+        EventManager.INSTANCE.post(new MouseInputEvent(buttonInfo.button(), action));
     }
 
     @Inject(method = "onMove", at = @At("HEAD"))
