40a987a36fe4478665f8599e90cfbee1530e6c7b
Julian Chang <wyvest@polyfrost.org>
2026-08-06 11:02:02 +0700

    Reapply "refactor: stop using Redirect"
    This reverts commit 2b69a01d7ba68344601cae84366d40ea20765f28.

--- minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/skia/Mixin_FixComposeRaceCondition.java
+++ minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/skia/Mixin_FixComposeRaceCondition.java
@@ -2,14 +2,15 @@ package org.polyfrost.oneconfig.internal.mixin.skia;
 
 import androidx.compose.ui.platform.GlobalSnapshotManager;
 import androidx.compose.ui.scene.BaseComposeScene;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BaseComposeScene.class)
 public class Mixin_FixComposeRaceCondition {
-    @Redirect(remap = false, method = "<init>", at = @At(value = "INVOKE", target = "Landroidx/compose/ui/platform/GlobalSnapshotManager;ensureStarted()V"))
-    void impl$redirectEnsureStartup(GlobalSnapshotManager instance) {
+    @WrapOperation(remap = false, method = "<init>", at = @At(value = "INVOKE", target = "Landroidx/compose/ui/platform/GlobalSnapshotManager;ensureStarted()V"))
+    void impl$redirectEnsureStartup(GlobalSnapshotManager instance, Operation<Void> original) {
         // This is done to prevent the startup of the GlobalSnapshotManager, which causes
         // issues when the Compose scene is not built in the AWT thread.
     }
