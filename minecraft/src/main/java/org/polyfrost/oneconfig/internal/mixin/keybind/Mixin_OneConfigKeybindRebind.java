ff948091d0b2e1a2d470c60f625f5b8c9d44461f
Saad Nadeem <saadndm.sn@gmail.com>
2026-09-04 01:23:57 -0400

    chore: fix stonecutter

--- minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/keybind/Mixin_OneConfigKeybindRebind.java
+++ minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/keybind/Mixin_OneConfigKeybindRebind.java
@@ -140,7 +140,7 @@ public class Mixin_OneConfigKeybindRebind implements OneConfigKeybindRecorder {
     @Override
     public void oneconfig$recordKey(int keyCode) {
         //? if >= 26.3
-        if (keyCode <= 0) return;
+        //if (keyCode <= 0) return;
         if (!oneconfig$begin()) return;
         oneconfig$keys.add(keyCode);
         oneconfig$preview();
