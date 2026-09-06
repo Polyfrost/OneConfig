1dc6beb44119f8869d457a4adfb68cd4b66fad97
Julian Chang <wyvest@polyfrost.org>
2026-09-06 15:07:53 -0400

    Revert "feat(ui): add onboarding"
    This reverts commit 3efc0eed7e133652c9ec2d20059ba35484169d00.

--- minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_ScreenOpenEvent_Fabric.java
+++ minecraft/src/main/java/org/polyfrost/oneconfig/internal/mixin/events/Mixin_ScreenOpenEvent_Fabric.java
@@ -2,7 +2,6 @@ package org.polyfrost.oneconfig.internal.mixin.events;
 
 //? fabric {
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import org.polyfrost.oneconfig.api.event.v1.EventManager;
 import org.polyfrost.oneconfig.api.event.v1.events.ScreenOpenEvent;
@@ -11,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-//~ if >= 26.2 'Minecraft.class' -> 'Gui.class'
-@Mixin(Gui.class)
+@Mixin(Minecraft.class)
 public class Mixin_ScreenOpenEvent_Fabric {
-    @Inject(method = "setScreen", at = @At("HEAD"), cancellable = true)
+    //~ if >= 26.2 'setScreen' -> 'setScreenAndShow'
+    @Inject(method = "setScreenAndShow", at = @At("HEAD"), cancellable = true)
     private void screenOpenCallback(Screen screen, CallbackInfo ci) {
         ScreenOpenEvent event = new ScreenOpenEvent(screen);
         EventManager.INSTANCE.post(event);
